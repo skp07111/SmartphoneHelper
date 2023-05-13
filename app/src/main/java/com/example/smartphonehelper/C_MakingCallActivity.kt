e82bbead4ccd777224ea2f9fca2f856f424d8a9b
catapillar0505 <twrp3301@gmail.com>
2023-05-14 03:57:09 +0900

    aa

--- app/src/main/java/com/example/smartphonehelper/C_MakingCallActivity.kt
+++ app/src/main/java/com/example/smartphonehelper/C_MakingCallActivity.kt
@@ -1,5 +1,6 @@
 package com.example.smartphonehelper
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
@@ -7,5 +8,7 @@ class C_MakingCallActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_call_makingcall)
+        //3초후 통화 연결 되어 통화중 화면(C_OnThePhoneCallActivity)으로 전환
+        //Handler().postDelayed({ startActivity(Intent(StartActivity::class.java, MainActivity::class.java)) }, 2000L)
     }
 }
