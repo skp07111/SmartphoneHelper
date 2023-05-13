6ffae64baf15f2448e37da847c56baf94229384f
skp07111 <skp07111@naver.com>
2023-05-14 02:07:41 +0900

    commit

---
+++ app/src/main/java/com/example/smartphonehelper/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.smartphonehelper
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    lateinit var mainCall : Button
+    lateinit var mainVideoCall : Button
+    lateinit var mainMessage : Button
+    lateinit var mainCamera : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // 전화걸기 버튼
+        mainCall = findViewById<Button>(R.id.button_call)
+        // 영상통화 버튼
+        mainVideoCall = findViewById<Button>(R.id.button_videoCall)
+        // 메시지 버튼
+        mainMessage = findViewById<Button>(R.id.button_message)
+        // 카메라 버튼
+        mainCamera = findViewById<Button>(R.id.button_camera)
+
+        // 전화걸기 버튼 누르면 CallActivity로 전환
+        mainCall.setOnClickListener() {
+            var intent = Intent(this, CallActivity::class.java)
+            startActivity(intent)
+        }
+
+        // 영상통화 버튼 누르면 VideoCallActivity로 전환
+        mainVideoCall.setOnClickListener() {
+            var intent = Intent(this, VideoCallActivity::class.java)
+            startActivity(intent)
+        }
+
+        // 메시지 버튼 누르면 MessageActivity로 전환
+        mainMessage.setOnClickListener() {
+            var intent = Intent(this, MessageActivity::class.java)
+            startActivity(intent)
+        }
+
+        // 카메라 버튼 누르면 CameraActivity로 전환
+        mainCamera.setOnClickListener() {
+            var intent = Intent(this, CameraActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/smartphonehelper/VideoCallActivity.kt
@@ -0,0 +1,21 @@
+package com.example.smartphonehelper
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class VideoCallActivity : AppCompatActivity() {
+
+    lateinit var videoCall: Button
+    lateinit var call: Button
+    lateinit var back: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_video_call)
+
+        videoCall = findViewById<Button>(R.id.icon_video)
+        call = findViewById<Button>(R.id.icon_call)
+        back = findViewById<Button>(R.id.icon_back)
+    }
+}
