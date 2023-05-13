372c6efb46dc1cc8bf86265ed8703783001127fe
seoyoung <si159521@gmail.com>
2023-05-14 03:55:06 +0900

    camera

--- app/src/main/java/com/example/smartphonehelper/MessageMenu.kt
+++ app/src/main/java/com/example/smartphonehelper/MessageMenu.kt
@@ -1,14 +1,12 @@
 package com.example.smartphonehelper
 
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
-class MessageActivity : AppCompatActivity(){
-    
+class MessageMenu : AppCompatActivity(){
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_message)
-    }
+        setContentView(R.layout.activity_message_menu)}
 }
