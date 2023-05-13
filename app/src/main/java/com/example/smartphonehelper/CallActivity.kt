4b13cf05bc08a88bb06d773f1d18cc4546c80d67
catapillar0505 <twrp3301@gmail.com>
2023-05-14 03:46:12 +0900

    aa

--- app/src/main/java/com/example/smartphonehelper/CallActivity.kt
+++ app/src/main/java/com/example/smartphonehelper/CallActivity.kt
@@ -1,5 +1,6 @@
 package com.example.smartphonehelper
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
@@ -17,6 +18,12 @@ class CallActivity : AppCompatActivity() {
         videoCall = findViewById<Button>(R.id.icon_video)
         call = findViewById<Button>(R.id.icon_call)
         back = findViewById<Button>(R.id.icon_back)
+
+        // 통화 버튼 눌렀을 때 통화거는중 화면 (activity_call_makingcall)으로 전환
+        call.setOnClickListener {
+            val intent = Intent(this, C_MakingCallActivity::class.java)
+            startActivity(intent)
+        }
     }
 
 }
