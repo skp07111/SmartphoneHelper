997c253a54c4121e282df4da7990bd02c5a998de
catapillar0505 <twrp3301@gmail.com>
2023-05-14 21:12:22 +0900

    aa

--- app/src/main/java/com/example/smartphonehelper/C_MakingCallActivity.kt
+++ app/src/main/java/com/example/smartphonehelper/C_MakingCallActivity.kt
@@ -3,12 +3,17 @@ package com.example.smartphonehelper
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Handler;
+import android.os.Looper
 
 class C_MakingCallActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_call_makingcall)
         //3초후 통화 연결 되어 통화중 화면(C_OnThePhoneCallActivity)으로 전환
-        //Handler().postDelayed({ startActivity(Intent(StartActivity::class.java, MainActivity::class.java)) }, 2000L)
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, C_OnThePhoneCallActivity::class.java))
+        }, 3000)
+        
     }
 }
--- app/src/main/java/com/example/smartphonehelper/C_OnThePhoneCallActivity.kt
+++ app/src/main/java/com/example/smartphonehelper/C_OnThePhoneCallActivity.kt
@@ -1,11 +1,21 @@
 package com.example.smartphonehelper
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class C_OnThePhoneCallActivity : AppCompatActivity() {
+    lateinit var stopCall: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_call_onthephone)
+        stopCall = findViewById<Button>(R.id.btn_stopCall)
+
+        // 통화 끊기 버튼을 누르면 통화종료되어 CallActivity 화면으로 전환
+        stopCall.setOnClickListener {
+            val intent = Intent(this, CallActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
