c09cd6c35f29d8bc83344cc7258481cba86fd0a1
박주원 <juwon_0715@naver.com>
2023-05-17 00:32:01 +0900

    camera

--- app/src/main/java/com/example/smartphonehelper/CameraActivity.kt
+++ app/src/main/java/com/example/smartphonehelper/CameraActivity.kt
@@ -1,14 +1,14 @@
 package com.example.smartphonehelper
 
 import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Matrix
 import android.hardware.Camera
 import android.os.Bundle
-import android.view.SurfaceHolder
-import android.view.SurfaceView
-import android.view.View
+import android.view.*
 import android.widget.Button
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
@@ -45,11 +45,11 @@ class CameraActivity : AppCompatActivity(), SurfaceHolder.Callback {
     private fun captureImage() {
         camera?.takePicture(null, null, Camera.PictureCallback { data, _ ->
             val bitmap = data?.toBitmap()
-            capturedImage.setImageBitmap(bitmap)
+            val rotatedBitmap = rotateBitmap(bitmap, 90) // 이미지를 90도 회전시킴
+            capturedImage.setImageBitmap(rotatedBitmap)
             capturedImage.visibility = View.VISIBLE
             surfaceHolder?.let { holder ->
                 try {
-                    val surface = holder.surface
                     camera?.stopPreview()
                     camera?.setPreviewDisplay(holder)
                     camera?.startPreview()
@@ -59,6 +59,15 @@ class CameraActivity : AppCompatActivity(), SurfaceHolder.Callback {
             }
         })
     }
+
+    private fun rotateBitmap(bitmap: Bitmap?, degrees: Int): Bitmap? {
+        val matrix = Matrix()
+        matrix.postRotate(degrees.toFloat())
+        return Bitmap.createBitmap(
+            bitmap!!, 0, 0, bitmap.width, bitmap.height, matrix, true
+        )
+    }
+
 
     private fun ByteArray.toBitmap(): Bitmap? {
         return BitmapFactory.decodeByteArray(this, 0, this.size)
@@ -89,11 +98,29 @@ class CameraActivity : AppCompatActivity(), SurfaceHolder.Callback {
         if (surfaceHolder?.surface == null) {
             return
         }
+
         try {
             camera?.stopPreview()
         } catch (e: Exception) {
             e.printStackTrace()
         }
+
+        val parameters = camera?.parameters
+        val previewSize = getOptimalPreviewSize(parameters, height, width) // 미리보기의 가로세로를 교체하여 설정합니다.
+        parameters?.setPreviewSize(previewSize?.width ?: width, previewSize?.height ?: height)
+
+        // 미리보기 회전 설정
+        val display = (getSystemService(Context.WINDOW_SERVICE) as WindowManager).defaultDisplay
+        val rotation = when (display.rotation) {
+            Surface.ROTATION_0 -> 90
+            Surface.ROTATION_90 -> 0
+            Surface.ROTATION_180 -> 270
+            Surface.ROTATION_270 -> 180
+            else -> 0
+        }
+        camera?.setDisplayOrientation(rotation)
+
+        camera?.parameters = parameters
 
         try {
             camera?.setPreviewDisplay(surfaceHolder)
@@ -101,6 +128,28 @@ class CameraActivity : AppCompatActivity(), SurfaceHolder.Callback {
         } catch (e: IOException) {
             e.printStackTrace()
         }
+    }
+
+    private fun getOptimalPreviewSize(
+        parameters: Camera.Parameters?,
+        width: Int,
+        height: Int
+    ): Camera.Size? {
+        val supportedSizes = parameters?.supportedPreviewSizes
+        val targetRatio = width.toDouble() / height
+        var optimalSize: Camera.Size? = null
+        var minDiff = Double.MAX_VALUE
+
+        for (size in supportedSizes!!) {
+            val ratio = size.width.toDouble() / size.height
+            val diff = Math.abs(ratio - targetRatio)
+            if (diff < minDiff) {
+                optimalSize = size
+                minDiff = diff
+            }
+        }
+
+        return optimalSize
     }
 
     override fun surfaceDestroyed(holder: SurfaceHolder) {
