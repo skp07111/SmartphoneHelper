ae68f3cba4a604dd1f597815cd7b101bd9ccddf1
skp07111 <skp07111@naver.com>
2023-05-17 13:13:40 +0900

    영상통화 TTS

--- app/src/main/java/com/example/smartphonehelper/VC_MakingCallActivity.kt
+++ app/src/main/java/com/example/smartphonehelper/VC_MakingCallActivity.kt
@@ -12,6 +12,7 @@ import android.hardware.camera2.*
 import android.media.MediaRecorder.VideoSource.CAMERA
 import android.os.Bundle
 import android.provider.MediaStore
+import android.speech.tts.TextToSpeech
 import android.util.Log
 import android.view.Surface
 import android.view.SurfaceHolder
@@ -20,6 +21,7 @@ import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import java.util.*
 
 class VC_MakingCallActivity  : AppCompatActivity() {
 
@@ -29,6 +31,7 @@ class VC_MakingCallActivity  : AppCompatActivity() {
     private lateinit var surfaceHolder: SurfaceHolder
     private lateinit var captureSession: CameraCaptureSession
     private lateinit var previewSurface: Surface
+    var tts: TextToSpeech? = null
 
     private val cameraDeviceStateCallback = object : CameraDevice.StateCallback() {
         override fun onOpened(camera: CameraDevice) {
@@ -88,71 +91,94 @@ class VC_MakingCallActivity  : AppCompatActivity() {
         } else {
             openCamera()
         }
+
+        openTTS()
     }
 
-    private fun openCamera() {
-        try {
-            val cameraId = cameraManager.cameraIdList[0]
-            if (ActivityCompat.checkSelfPermission(
-                    this,
-                    Manifest.permission.CAMERA
-                ) != PackageManager.PERMISSION_GRANTED
-            ) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return
-            }
-            cameraManager.openCamera(cameraId, cameraDeviceStateCallback, null)
-        } catch (e: CameraAccessException) {
-            Log.e("VC_MakingCallActivity", "Failed to access camera", e)
-        }
-    }
-
-    private fun createCameraPreviewSession() {
-        try {
-            cameraDevice.createCaptureSession(listOf(previewSurface), object : CameraCaptureSession.StateCallback() {
-                override fun onConfigured(session: CameraCaptureSession) {
-                    captureSession = session
-                    updatePreview()
+        private fun openTTS() {
+            tts = TextToSpeech(applicationContext) { status ->
+                if (status == TextToSpeech.SUCCESS) {
+                    val result = tts?.setLanguage(Locale.KOREAN)
+                    if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                        Log.e("TTS", "Korean language is not supported.")
+                    }
+                } else {
+                    Log.e("TTS", "Initialization failed.")
                 }
-
-                override fun onConfigureFailed(session: CameraCaptureSession) {
-                    Log.e("VC_MakingCallActivity", "Failed to configure camera capture session")
-                }
-            }, null)
-        } catch (e: CameraAccessException) {
-            Log.e("VC_MakingCallActivity", "Failed to create camera capture session", e)
-        }
-    }
-
-    private fun updatePreview() {
-        try {
-            val captureRequestBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
-            captureRequestBuilder.addTarget(previewSurface)
-            captureSession.setRepeatingRequest(captureRequestBuilder.build(), null, null)
-        } catch (e: CameraAccessException) {
-            Log.e("VC_MakingCallActivity", "Failed to update preview", e)
-        }
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                openCamera()
-            } else {
-                Log.e("VC_MakingCallActivity", "Camera permission denied")
+                //tts 말하기
+                tts?.speak(
+                    "이 기능은 영상통화 기능입니다.",
+                    TextToSpeech.QUEUE_FLUSH,
+                    null,
+                    null
+                )
             }
         }
+
+        private fun openCamera() {
+            try {
+                val cameraId = cameraManager.cameraIdList[0]
+                if (ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.CAMERA
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return
+                }
+                cameraManager.openCamera(cameraId, cameraDeviceStateCallback, null)
+            } catch (e: CameraAccessException) {
+                Log.e("VC_MakingCallActivity", "Failed to access camera", e)
+            }
+        }
+
+        private fun createCameraPreviewSession() {
+            try {
+                cameraDevice.createCaptureSession(listOf(previewSurface), object : CameraCaptureSession.StateCallback() {
+                    override fun onConfigured(session: CameraCaptureSession) {
+                        captureSession = session
+                        updatePreview()
+                    }
+
+                    override fun onConfigureFailed(session: CameraCaptureSession) {
+                        Log.e("VC_MakingCallActivity", "Failed to configure camera capture session")
+                    }
+                }, null)
+            } catch (e: CameraAccessException) {
+                Log.e("VC_MakingCallActivity", "Failed to create camera capture session", e)
+            }
+        }
+
+        private fun updatePreview() {
+            try {
+                val captureRequestBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
+                captureRequestBuilder.addTarget(previewSurface)
+                captureSession.setRepeatingRequest(captureRequestBuilder.build(), null, null)
+            } catch (e: CameraAccessException) {
+                Log.e("VC_MakingCallActivity", "Failed to update preview", e)
+            }
+        }
+
+        override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+            if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    openCamera()
+                } else {
+                    Log.e("VC_MakingCallActivity", "Camera permission denied")
+                }
+            }
+        }
+
+        override fun onDestroy() {
+            super.onDestroy()
+            cameraDevice.close()
+        }
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        cameraDevice.close()
-    }
-}
