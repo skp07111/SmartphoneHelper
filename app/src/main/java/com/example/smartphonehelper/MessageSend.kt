4f9a01534deff7711291c2cb61fc7098dd6176ab
seoyoung <si159521@gmail.com>
2023-05-21 23:47:58 +0900

    message

---
+++ app/src/main/java/com/example/smartphonehelper/MessageSend.kt
@@ -0,0 +1,14 @@
+package com.example.smartphonehelper
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MessageSend : AppCompatActivity() {
+    //메세지 보내기 성공
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sendmessage)
+    }
+}
--- app/src/main/java/com/example/smartphonehelper/MessageWrite.kt
+++ app/src/main/java/com/example/smartphonehelper/MessageWrite.kt
@@ -1,10 +1,19 @@
 package com.example.smartphonehelper
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MessageWrite : AppCompatActivity(){
+    lateinit var send: Button
+    //종이비행기 버튼 누르면 화면전환(메세지 보내기 성공)
     override fun onCreate(savedInstanceState: Bundle?) {
+        send = findViewById<Button>(R.id.btn_send)
+        send.setOnClickListener{
+            val intent= Intent(this, MessageSend::class.java)
+            startActivity(intent)
+        }
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_writethemessage)
 
