0f8c404996bf43076eed38b696c0017f0fd64702
박주원 <juwon_0715@naver.com>
2023-05-24 04:26:49 +0900

    gallery

--- app/src/main/java/com/example/smartphonehelper/GalleryActivity.kt
+++ app/src/main/java/com/example/smartphonehelper/GalleryActivity.kt
@@ -1,34 +1,39 @@
 package com.example.smartphonehelper
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.RelativeLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class GalleryActivity : AppCompatActivity() {
 
+    private lateinit var popupContainer: RelativeLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gallery)
 
-        showPopupMessage("갤러리에 있는 앨범은 사진을 분류하여 모아줍니다.")
+        showPopupMessage("갤러리에 있는 앨범은\n사진을 각각 분류하여\n모아주는 곳입니다.")
     }
 
     private fun showPopupMessage(message: String) {
-        val inflater = LayoutInflater.from(this)
-        val layout = inflater.inflate(R.layout.popup_message, findViewById<ViewGroup>(R.id.popupContainer), false)
+        popupContainer = findViewById(R.id.popupContainer)
+        val messageTextView = findViewById<TextView>(R.id.messageTextView)
 
-        val messageTextView = layout.findViewById<TextView>(R.id.messageTextView)
         messageTextView.text = message
-
-        val toast = Toast(applicationContext)
-        toast.setGravity(Gravity.TOP or Gravity.CENTER_HORIZONTAL, 0, 200)
-        toast.duration = Toast.LENGTH_LONG
-        toast.view = layout
-        toast.show()
+        popupContainer.visibility = View.VISIBLE
     }
+
+    private fun closePopupMessage() {
+        popupContainer.visibility = View.GONE
+    }
+
 }
