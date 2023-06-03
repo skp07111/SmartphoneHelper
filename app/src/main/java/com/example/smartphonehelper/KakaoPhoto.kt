5c3f0f9dea54d0ff7e737c3cbed32e510fd6d7f0
seoyoung <si159521@gmail.com>
2023-06-03 23:27:11 +0900

    kakao

---
+++ app/src/main/java/com/example/smartphonehelper/KakaoPhoto.kt
@@ -0,0 +1,12 @@
+package com.example.smartphonehelper
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class KakaoPhoto: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kakao_send_photo)
+    }
+
+}
