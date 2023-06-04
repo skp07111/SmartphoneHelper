80123a98821bab7af1ea59c794d3792b858f7517
seoyoung <si159521@gmail.com>
2023-06-05 03:27:59 +0900

    kakao

--- app/src/main/java/com/example/smartphonehelper/MessageWrite.kt
+++ app/src/main/java/com/example/smartphonehelper/MessageWrite.kt
@@ -9,12 +9,14 @@ class MessageWrite : AppCompatActivity(){
     lateinit var send: Button
     //종이비행기 버튼 누르면 화면전환(메세지 보내기 성공)
     override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_writethemessage)
+
         send = findViewById<Button>(R.id.btn_send)
         send.setOnClickListener{
             val intent= Intent(this, MessageSend::class.java)
             startActivity(intent)
         }
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_writethemessage)
+
     }
 }
