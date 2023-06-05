8eb215cc5fcbcb40f5a7cbf443c54ec0e362f624
박주원 <juwon_0715@naver.com>
2023-06-05 20:08:06 +0900

    playStore

--- app/src/main/java/com/example/smartphonehelper/PlayStore3Activity.kt
+++ app/src/main/java/com/example/smartphonehelper/PlayStore3Activity.kt
@@ -4,22 +4,23 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.ImageButton
 import android.widget.RelativeLayout
 import android.widget.TextView
 
-class PlayStore2Activity : AppCompatActivity() {
+class PlayStore3Activity : AppCompatActivity() {
     private lateinit var popupContainer: RelativeLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_play_store2)
+        setContentView(R.layout.activity_play_store3)
 
-        showPopupMessage("검색창에서 설치하고 싶은 앱을\n직접 입력할 수 있습니다.\n혹은 왼쪽 상단에 [마이크] 모양 버튼을 눌러서\n음성 검색을 할 수 있어요.\n한 번 눌러보세요.")
+        showPopupMessage("실제 Play 스토어 앱에 들어가서 연습해 보세요!")
 
-        val playStoreMikeIcon = findViewById<ImageButton>(R.id.playStoreMikeIcon)
-        playStoreMikeIcon.setOnClickListener {
-            val intent = Intent(this, PlayStore3Activity::class.java)
+        val appDownloadButton = findViewById<Button>(R.id.appDownloadButton)
+        appDownloadButton.setOnClickListener {
+            val intent = Intent(this, PlayStore4Activity::class.java)
             startActivity(intent)
         }
     }
---
+++ app/src/main/java/com/example/smartphonehelper/PlayStore4Activity.kt
@@ -0,0 +1,11 @@
+package com.example.smartphonehelper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlayStore4Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_play_store4)
+    }
+}
